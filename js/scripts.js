b7729a7f196e54951e7cef9e868fcd3b44d4b71a
Student <example@example.com>
2019-10-24 16:43:28 -0700

    add html,css,js scripts for roll coster section js hide show elements

--- js/scripts.js
+++ js/scripts.js
@@ -21,6 +21,22 @@ $(document).ready(function(){
 
   });
 
+  var height = parseInt(prompt("How tall are you in inches?"));
 
+    if (height <48) {
+      $(".death-coaster").hide();
+      $(".baby-coaster").show();
+      $(".goldilocks-coaster").hide();
+    } else if (height > 83){
+      $(".death-coaster").hide();
+      $(".baby-coaster").hide();
+      $(".goldilocks-coaster").show();
+    } else {
+      $(".death-coaster").show();
+      $(".baby-coaster").hide();
+      $(".goldilocks-coaster").show();
+    }
 
 });
+
+// });
